b1c5a3d36635cad8ad5591efa1ba580318382beb
Abhishek Yadav <abhishekgit1208@gmail.com>
2018-09-23 13:55:20 +0530

    file 1 and 2 changed

--- file1.js
+++ file1.js
@@ -1,4 +1,5 @@
 function fun(){
     console.log("This is my first file");
     console.log("And this is my first change in this file!")
+    console.log("why are you changing me?")
 }
--- file2.js
+++ file2.js
@@ -1,2 +1,3 @@
 console.log("Happy to use git!")
 console.log("This is my first change in this file")
+console.log("why are you changing me?")
