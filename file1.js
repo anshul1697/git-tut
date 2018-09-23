f4e81f7a8c86bb386f1163163029981f4b49e1fe
Abhishek Yadav <abhishekgit1208@gmail.com>
2018-09-23 14:01:25 +0530

    feature 1 added

--- file1.js
+++ file1.js
@@ -2,4 +2,5 @@ function fun(){
     console.log("This is my first file");
     console.log("And this is my first change in this file!")
     console.log("why are you changing me?")
+    console.log("Feature 1 added")
 }
